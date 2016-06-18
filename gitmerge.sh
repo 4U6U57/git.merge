ab513742bc1d2461c971b2539ed8cc0ced4cbedf
August Valera <4U6U57@gmail.com>
2016-06-18 16:20:48 -0700

    Works well, might want to edit commit messages and prompts, but pretty much done

--- gitmerge.sh
+++ gitmerge.sh
@@ -16,57 +16,54 @@ Exit() {
    exit
 }
 trap "Exit" SIGHUP SIGINT SIGTERM
+Error() {
+   Echo "$(EchoColor Red)$@$(EchoColor)"
+   Exit
+}
 
 # Echo Configuration
 source $ExeDir/Echo.sh
 EchoPrefix="$(EchoColor Cyan)$ExeName$(EchoColor):"
+ReadPrefix="$(EchoColor Yellow)$ExeName$(EchoColor)$"
 
 # Program start
 Echo "$ExeName - Git repository merging utility"
 
-case "$-" in
-   *i*)
-      Echo "Running in interactive mode"
-      ;;
-   *)
-      Echo "Running in script mode"
-      ;;
-esac
-
 # Find base repo
 BaseName=""
-if [[ -e .git ]]; then
-   bash
-   BaseName=$(basename pwd)
-   Echo "Currently in git repo $BaseName. Would you like to use this as your base? (y,n)"
-   Read -p $EchoPrefix Input
-   if [[ $Input != "y" ]]; then
-      Echo "Please change directories, and try again."
-      Exit
-   fi
-   Echo "Setting up $BaseName as base."
+Echo "Enter in the name of the base repo directory to be created. (DIRECTORY)"
+Read -p $ReadPrefix -t 15 BaseName
+if [[ $BaseName == "" ]]; then
+   Error "Invalid name for directory"
+elif [[ $BaseName == "." ]]; then
+   Echo "Using the current directory as the base repo directory."
+   BaseDir=$(pwd)
+   BaseName=$(basename $BaseDir)
+elif [[ -e $BaseName ]]; then
+   Error "Directory $BaseName already exists"
 else
-   Echo "No base git repo detected, creating. What would you like to call it? (DIRECTORY)"
-   Read -p $EchoPrefix BaseName
-   if [[ $BaseName == "" ]]; then
-      Echo "Invalid name"
-      Exit
-   fi
    Echo "Setting up $BaseName as new git base."
    mkdir $BaseName
    cd $BaseName
+   BaseDir=$(pwd)
+fi
+
+# Initializing base repo, if necessary
+if [[ ! -e .git/ ]]; then
    git init
    echo -e "# $BaseName\nBase git repo created with $ExeName" > README
    git add -A
    git commit -m "[$ExeName] Initial commit"
 fi
 
+# Merge in repos
 while Boolean $True; do
+   cd $BaseDir
    ImportDir=".Import"
    rm -rf $ImportDir
    Echo "Ready for the next repo to merge into $BaseName. Enter in a URL
    (SSH/HTML), or nothing to finish. (URL [DIRECTORY])"
-   Read -p $EchoPrefix RepoUrl RepoNameTemp
+   Read -p $ReadPrefix -t 30 RepoUrl RepoNameTemp
    if [[ $RepoUrl == "" ]]; then
       break
    fi
@@ -75,6 +72,7 @@ while Boolean $True; do
    else
       RepoName=$(basename $(basename $RepoUrl) .git)
    fi
+   Echo "Cloning in $RepoUrl into $RepoName"
    git clone $RepoUrl $ImportDir
    if [[ -e $ImportDir ]]; then
       cd $ImportDir
@@ -91,6 +89,18 @@ while Boolean $True; do
       git add -A
       git commit -m "[$ExeName] Finalizing $RepoName"
    else
-      Echo "Error cloning in $RepoUrl"
+      rm -rf $BaseDir
+      Error "Error cloning in $RepoUrl, aborting"
    fi
 done
+
+# Finishing
+cd $BaseDir
+Echo "Successfully processed $BaseName"
+Echo "Enter in a remote repository URL to push the merged repo to, or nothing to finish. (URL)"
+Read -p $ReadPrefix -t 30 BaseUrl
+if [[ $BaseUrl != "" ]]; then
+   git remote add origin $BaseUrl
+   git push origin master
+fi
+Exit
